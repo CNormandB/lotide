6f60e5d556111d6fd7c6b127650426aca4de3ca1
Casandra Normand <casandra.normand@outlook.com>
2023-07-26 15:26:42 -0400

    Modified assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
- if(actual === expected){
-  console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
- } else console.log(`❌ Assertion Failed: ${actual}, !==  ${expected}`);
-}
-
+  if (actual === expected) {
+    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`❌ Assertion Failed: ${actual}, !==  ${expected}`);
+};
+assertEqual();
 /*Test code
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1); 
+assertEqual(1, 1);
 */
