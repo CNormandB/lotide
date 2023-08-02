1849f2adc81d7b2678f0f1cdb07ff9a6e69157f4
Casandra Normand <casandra.normand@outlook.com>
2023-08-02 17:01:57 -0400

    converted assertArraysEqual.js w M&C

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,22 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 3, 4], [1, 3, 4]);
-assertArraysEqual([1,3,6], [1,3,'6']);
-assertArraysEqual([1,3,6], []);
-assertArraysEqual([], []);
+describe("#assertArraysEqual", () => {
+  it("returns [1, 2, 3] for [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+
+  it("returns failed [1, 3, 6] for [1, 3, '6']", () => {
+    assert.strictEqual(assertArraysEqual([1, 3, 6], [1, 3, '6'], 'Arrays not the same'));
+  });
+
+  it("returns failed for [1, 3, 6]", () => {
+    assert.strictEqual(assertArraysEqual([1, 3, 6], [], 'Arrays not the same'));
+  });
+
+  it("returns [] for []", () => {
+    assert.strictEqual(assertArraysEqual([], []));
+  });
+
+});
