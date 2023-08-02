4e3af275d21ee04f512edcba72631af043b0c994
Casandra Normand <casandra.normand@outlook.com>
2023-08-02 19:21:07 -0400

    Fixed issue where index was used instead of value

--- middle.js
+++ middle.js
@@ -1,18 +1,18 @@
-const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   // if array has one or 2 element(s) = []
   if (array.length < 2) {
     return [];
-    //if array has odd # of elements = array with single element
-  } else if (array.length % 2 === 0) {
+   
+  } //if array has even # of elements = array with two middle elements
+  else if (array.length % 2 === 0) {
     const middleNumber = (Math.floor(array.length / 2));
-    return middleNumber;
-  //if array has even # of elements = array with two middle elements
-  } else {
+    return [array[middleNumber - 1], array[middleNumber]];
+  } //if array has odd # of elements = array with single element
+  else {
     const middleNumber = (Math.floor(array.length / 2));
-    return [middleNumber, middleNumber + 1];
+    return [array[middleNumber]];
   }
 };
 
