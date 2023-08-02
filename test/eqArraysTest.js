c3d6dc49fc2b92c07eaa81da8fef27591e581115
Casandra Normand <casandra.normand@outlook.com>
2023-08-02 17:33:44 -0400

    converted eqArraysTest.js to M&C

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,25 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+describe("#eqArrays", () => {
+  it("returns true if arrays are equal", () => {
+    const actual = [1, 2, 3];
+    const expected = [1, 2, 3]
+    assert.isTrue(eqArrays(actual, expected));
+  })
+
+  it("returns false if the arrays are different", () => {
+    const actual = [1, 2, 3];
+    const expected = [1, 2, '3'];
+    assert.isFalse(eqArrays(actual, expected));
+  })
+
+  it("returns true if the arrays are the same", () => {
+    assert.isTrue(eqArrays([], []));
+  })
+})
+
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
