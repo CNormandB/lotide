3fc3b25e4476f8a417b01bd89ecfb2678af66c6b
Casandra Normand <casandra.normand@outlook.com>
2023-08-02 00:29:32 -0400

    refactored eqArrays.js, created eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`❌ Assertion Failed: ${actual}, !==  ${expected}`);
-};
+const assertEqual = require('./assertEqual');
+
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -16,6 +13,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+module.exports = eqArrays;
