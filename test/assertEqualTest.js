a55d12736d4de86b8a5bc8937c673733932fa5f8
Casandra Normand <casandra.normand@outlook.com>
2023-08-02 17:22:17 -0400

    converted assertEqualsTest.js to M&C

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,16 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+describe("#assertEqual", () => {
+  it("returns false when they aren't the same", () => {
+    const expected = "Lighthouse Labs";
+    const actual = "Bootcamp";
+   assert.strictEqual(assertEqual(expected, actual));
+  })
+
+  it("returns true when they are the same", () => {
+    const expected = 1;
+    const actual = 1;
+    assert.strictEqual(assertEqual(expected, actual));
+   })
+});
